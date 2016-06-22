0db618cb3a8ef5c7547e7722b26f6f8e6a02884e
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-06-22 00:54:41 -0300

    Max Sum Sub Matrix With Transpose case

--- src/MaxSumSubMatrix.java
+++ src/MaxSumSubMatrix.java
@@ -13,8 +13,19 @@ public class MaxSumSubMatrix {
         System.out.print(v);
     }
 
+    private int[][] transpose(int[][] matrix){
+        int[][] transposedMatrix = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposedMatrix;
+    }
+
     public int maxSumSubmatrix(int[][] matrix, int k) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0)   return 0;
+        if(matrix.length < matrix[0].length)    return maxSumSubmatrix(transpose(matrix), k);
         int[][] sumMatrix = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
