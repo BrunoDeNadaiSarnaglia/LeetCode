4eb31583ba3b08f3fd6db033697c5f9ec2a7b27f
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-07-16 20:54:58 -0300

    Higher To Small

---
+++ src/HigherToSmall.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Bruno on 7/16/2016.
+ */
+public class HigherToSmall {
+    public int guessNumber(int n){
+        return guessNumber(1, n);
+    }
+
+    private int guessNumber(int i, int j){
+        if(i == j)  return i;
+        int mid = i + (j - i) / 2;
+        int result = guess(mid);
+        if(result == 0) return mid;
+        if(result == 1) return guessNumber(mid + 1, j);
+        return guessNumber(i, mid - 1);
+    }
+}
