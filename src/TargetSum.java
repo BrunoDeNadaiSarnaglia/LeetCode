fe7722f9142c1f563432fc92218b2cd11b1079ea
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2017-01-22 12:57:46 -0300

    Target Sum

--- src/TargetSum.java
+++ src/TargetSum.java
@@ -4,22 +4,23 @@
 public class TargetSum {
 
     public int findTargetSumWays(int[] nums, int S) {
-        int DEFAULT = 1001;
-        int[] cache = new int[2 * DEFAULT];
-        cache[DEFAULT] = 1;
+        int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            int[] newCache = new int[2 * DEFAULT];
-            for (int j = 2 * DEFAULT - 1; j >= Math.abs(nums[i]); j--) {
-                newCache[j] += cache[j - Math.abs(nums[i])];
-            }
-            for (int j = 0; j < 2 * DEFAULT - Math.abs(nums[i]); j++) {
-                newCache[j] += cache[j + Math.abs(nums[i])];
-            }
-            for (int j = 0; j < 2 * DEFAULT; j++) {
-                cache[i] += newCache[j];
+            sum += nums[i];
+        }
+        if(S > sum || (sum + S) % 2 == 1)   return 0;
+        return countPossibleSums(nums, (sum + S) / 2);
+    }
+
+    private int countPossibleSums(int[] nums, int S){
+        int[] dp = new int[S + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = S; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
             }
         }
-        return (DEFAULT + S >= 2 * DEFAULT || DEFAULT + S < 0 ? 0 : cache[DEFAULT + S]);
+        return dp[S];
     }
 
 }
