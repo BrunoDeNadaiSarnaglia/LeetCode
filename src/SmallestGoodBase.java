7485549674d1436c42317e5c50a150c49fb6334c
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2017-01-25 02:03:22 -0300

    Smallest good base

---
+++ src/SmallestGoodBase.java
@@ -0,0 +1,49 @@
+/**
+ * Created by Sarnaglia on 1/25/17.
+ */
+public class SmallestGoodBase {
+
+    public static void main(String[] args){
+        SmallestGoodBase sb = new SmallestGoodBase();
+//        System.out.println(sb.smallestGoodBase("15"));
+//        System.out.println(sb.smallestGoodBase("4681"));
+        System.out.println(sb.smallestGoodBase("1000000000000000000"));
+    }
+
+    public String smallestGoodBase(String n) {
+        long val = Long.parseLong(n);
+        long ans = 0;
+        for (int countOnes = 2; countOnes < 18; countOnes++) {
+            long j = 2;
+            long k = val - 1;
+            while (k >= j){
+                long mid = j + (k - j) / 2;
+                long oneNumber = getOneBasedNum(mid, countOnes);
+                if(oneNumber < 0 || oneNumber > val){
+                    k = mid - 1;
+                } else {
+                    j = mid + 1;
+                }
+            }
+            if(k == 1)  break;
+            if(getOneBasedNum(k, countOnes) == val){
+                ans = k;
+            }
+        }
+        return Long.toString(ans);
+    }
+
+
+    private long getOneBasedNum(long base, long countOnes){
+        long accFactor = 1;
+        long sum = 0;
+        for (int i = 0; i < countOnes; i++) {
+            if(sum > Long.MAX_VALUE - accFactor) return -1;
+            sum += accFactor;
+            if(i != countOnes - 1 && base >= Long.MAX_VALUE / accFactor) return -1;
+            accFactor *= base;
+        }
+        return sum;
+    }
+
+}
