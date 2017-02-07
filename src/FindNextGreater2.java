67acb26c38e5cfe4bbe58ddaffda34151b9d042c
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2017-02-07 20:59:10 -0300

    FindNextGreater2

---
+++ src/FindNextGreater2.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by Sarnaglia on 2/7/17.
+ */
+public class FindNextGreater2 {
+
+
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stk = new Stack<>();
+        int[] nums2 = new int[nums.length];
+        int i = 0;
+        int max = 0;
+        while (i < nums.length + max || !stk.isEmpty()) {
+            max = (nums[i % nums.length] > nums[max] ? i : max);
+            if(i == nums.length + max){
+                nums2[stk.pop()] = -1;
+            } else if(stk.isEmpty() || nums[stk.peek()] > nums[i % nums.length]){
+                if(i < nums.length) stk.push(i % nums.length);
+                i++;
+            } else {
+                nums2[stk.pop()] = nums[i % nums.length];
+            }
+        }
+        return nums2;
+    }
+
+}
