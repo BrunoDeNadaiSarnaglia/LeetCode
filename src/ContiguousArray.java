2731dd3d5f4b1039495e78375ed865a233bc37a6
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2017-02-21 21:34:17 -0300

    ContiguousArray

--- src/ContiguousArray.java
+++ src/ContiguousArray.java
@@ -1,24 +1,25 @@
+import java.util.HashMap;
+
 /**
  * Created by Sarnaglia on 2/20/17.
  */
 public class ContiguousArray {
 
-    public int ContiguousArray(int[] array) {
-        int max = 0;
-        int[] zerosCountAcc = zerosCountAcc(array);
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i; j <= array.length; j += 2) {
-                max = Math.max(max, ((j - i) / 2 == zerosCountAcc[j] - zerosCountAcc[i] ? j - i : 0));
-            }
-        }
-        return max;
+    public static void main(String[] args){
+        ContiguousArray ca = new ContiguousArray();
+        ca.ContiguousArray(new int[]{0, 1, 0});
     }
 
-    private int[] zerosCountAcc(int[] array) {
-        int[] zerosCount = new int[array.length + 1];
+    public int ContiguousArray(int[] array) {
+        int max = 0;
+        HashMap<Integer, Integer> preSumToFstIdx = new HashMap<>();
+        int preSum = 0;
+        preSumToFstIdx.put(preSum, 0);
         for (int i = 0; i < array.length; i++) {
-            zerosCount[i + 1] = zerosCount[i] + (array[i] == '0' ? 1 : 0);
+            preSum += (array[i] == 0 ? 1 : -1);
+            if(preSumToFstIdx.containsKey(preSum))  max = Math.max(max, i + 1 - preSumToFstIdx.get(preSum));
+            else    preSumToFstIdx.put(preSum, i + 1);
         }
-        return zerosCount;
+        return max;
     }
 }
