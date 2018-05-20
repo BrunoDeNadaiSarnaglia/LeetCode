462412b333e2a9047b0f7291841e0f4356da743f
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2018-05-19 23:32:24 -0300

    Daily Temperatures permutation

---
+++ src/DailyTemperatures.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class DailyTemperatures {
+  public int[] dailyTemperatures(int[] temperatures) {
+    Stack<Integer> stk = new Stack<>();
+    int[] waitToWarmerDays = new int[temperatures.length];
+    for (int i = 0; i < temperatures.length; i++) {
+      if(stk.isEmpty() || temperatures[stk.peek()] >= temperatures[i]) {
+        stk.push(i);
+      } else {
+        while(!stk.isEmpty() && temperatures[stk.peek()] < temperatures[i]) {
+          int day = stk.pop();
+          waitToWarmerDays[day] = i - day;
+        }
+        stk.push(i);
+      }
+    }
+    return waitToWarmerDays;
+  }
+}
