eb6030c7c721bb65581c61cbb7b70c04b56ff358
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2018-07-01 22:11:29 -0300

    LargestPlusSign

--- src/LargestPlusSign.java
+++ src/LargestPlusSign.java
@@ -1,11 +1,50 @@
+import java.util.HashSet;
+
 public class LargestPlusSign {
-
   public int orderOfLargestPlusSign(int N, int[][] mines) {
-    int[][] leftMostZeroes = new int[mines.length][mines[0].length];
-    int[][] rightMostZeroes = new int[mines.length][mines[0].length];
-    for (int i = 0; i < mi; i++) {
+    int[][] grid = new int[N][N];
 
+    for (int i = 0; i < N; i++) {
+      Arrays.fill(grid[i], N);
     }
+
+    for (int[] m : mines) {
+        grid[m[0]][m[1]] = 0;
+    }
+
+    for (int i = 0; i < N; i++) {
+      int l = -1;
+      for (int j = 0; j < N; j++) {
+        if(grid[i][j] == 0) { l = j; }
+        grid[i][j] = Math.min(grid[i][j], j - l);
+      }
+      int r = N;
+      for (int j = N - 1; j >= 0; j--) {
+        if(grid[i][j] == 0) { r = j; }
+        grid[i][j] = Math.min(grid[i][j], r - j);
+      }
+    }
+    for (int j = 0; j < N; j++) {
+      int t = -1;
+      for (int i = 0; i < N; i++) {
+        if(grid[i][j] == 0) { t = i; }
+        grid[i][j] = Math.min(grid[i][j], i - t);
+      }
+      int b = N;
+      for (int i = N - 1; i >= 0; i--) {
+        if(grid[i][j] == 0) { b = i; }
+        grid[i][j] = Math.min(grid[i][j], b - i);
+      }
+    }
+    int res = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            res = Math.max(res, grid[i][j]);
+        }
+    }
+
+    return res;
   }
 
 }
