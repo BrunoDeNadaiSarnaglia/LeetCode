36a640049908c7e63600754318440b8d000bb029
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2018-07-06 23:24:14 -0300

    MaximumWidthBinaryTree

--- src/MaximumWidthBinaryTree.java
+++ src/MaximumWidthBinaryTree.java
@@ -1,4 +1,8 @@
 import java.awt.List;
+import java.util.Deque;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
 
 public class MaximumWidthBinaryTree {
   public int widthOfBinaryTree(TreeNode root) {
@@ -14,5 +18,35 @@ public class MaximumWidthBinaryTree {
     max = Math.max(max, widthOfBinaryTree(node.left, level + 1, 2 * idx, leftMost));
     max = Math.max(max, widthOfBinaryTree(node.right, level + 1, 2 * idx + 1, leftMost));
     return max;
+    leftMost.
+  }
+
+  public int widthOfBinaryTree(TreeNode root) {
+    int max = 0;
+    if(root == null) return max;
+    Deque<TreeNode> lvl = new LinkedList<TreeNode>();
+    lvl.add(root);
+    while(lvl.size() != 0) {
+      max = Math.max(max, lvl.size());
+      lvl = nextLevel(lvl);
+    }
+    return max;
+  }
+
+  private Deque<TreeNode> nextLevel(Deque<TreeNode> stk) {
+    Deque<TreeNode> stkNextLevel = new LinkedList<TreeNode>();
+    for(TreeNode node : stk) {
+      if(node != null) {
+        stkNextLevel.addLast(node.left);
+        stkNextLevel.addLast(node.right);
+      } else if(node == null && !stk.isEmpty()) {
+        stkNextLevel.addLast(null);
+        stkNextLevel.addLast(null);
+      }
+    }
+    while(!stkNextLevel.isEmpty() && stkNextLevel.getLast() == null) {
+      stkNextLevel.removeLast();
+    }
+    return stkNextLevel;
   }
 }
